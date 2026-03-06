2a2d4b1d80b668bec804eceb690e103acdfde3d8
naresh33-r <nareshmarquez448@gmail.com>
2026-03-06 19:49:34 +0530

    updated

--- Family-Event-Reminder/src/main/java/com/example/Family/Event/Reminder/familyController/familyController.java
+++ Family-Event-Reminder/src/main/java/com/example/Family/Event/Reminder/familyController/familyController.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
-@CrossOrigin(origins = "*")
+@CrossOrigin(origins = "https://family-event-reminder-3mot.vercel.app/")
 @RestController
 public class familyController {
 @Autowired
